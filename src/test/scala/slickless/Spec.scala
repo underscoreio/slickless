6678a247e988989755f2655b6bc7dfa9a95d2dfa
Scala Steward <me@scala-steward.org>
2019-09-21 13:05:53 +0200

    Update scalatest to 3.2.0-M1

--- src/test/scala/slickless/Spec.scala
+++ src/test/scala/slickless/Spec.scala
@@ -2,8 +2,8 @@ package slickless
 
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.{Millis, Seconds, Span}
-import org.scalatest.Matchers
 import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
 
 abstract class Spec extends AnyFreeSpec with Matchers with ScalaFutures {
   implicit val patience = PatienceConfig(timeout = Span(30, Seconds), interval = Span(250, Millis))
